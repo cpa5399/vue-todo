d1499d082ac8e439bdb2c4b01aababb2d62a6682
Ali <cali@constantcontact.com>
2018-02-04 18:22:55 -0500

    Editing and deleting todos

---
+++ src/components/TodoList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <p>Completed Tasks: {{completedTasks()}}</p>
+    <p>Pending Tasks: {{pendingTasks()}}</p>
+
+    <todo v-on:delete-todo="deleteTodo" v-for="todo in todos" v-bind:todo="todo"></todo>
+  </div>
+</template>
+
+<script type = "text/javascript" >
+
+  import Todo from './Todo';
+
+  export default {
+    // Allows component to access todos property from main App component
+    props: ['todos'],
+    components: {
+      Todo
+    },
+    methods: {
+      completedTasks: function() {
+        return this.todos.filter(function(todo) { return todo.done === true }).length;
+      },
+      pendingTasks: function() {
+        return this.todos.filter(function(todo) { return todo.done === false }).length;
+      },
+      deleteTodo: function(item) {
+        const todoIndex = this.todos.indexOf(item);
+        this.todos.splice(item, 1);
+      }
+    }
+  };
+</script>
+<style>
+</style>
